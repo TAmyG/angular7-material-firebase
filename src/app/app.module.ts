443d002a1d8896892f3a18c53ab38a7db970356f
TAmy <tamy.vivas@gmail.com>
2019-04-15 12:32:58 -0600

    feat: Angular with filter table of Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatModule } from './mat/mat.module';
+import { ListCustomersComponent } from './components/list-customers/list-customers.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ListCustomersComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
